710b9e49e51a43aeca2171cef7bfbdad569a0907
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2022-11-04 21:47:56 +0900

    PowerSet.java

--- src/PowerSet.java
+++ src/PowerSet.java
@@ -1,6 +1,8 @@
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
 
 public class PowerSet {
     public static void main(String[] args) {
@@ -12,16 +14,61 @@ public class PowerSet {
     public static ArrayList<String> powerSet(String str){
 
         ArrayList<String> arrayList=new ArrayList<>();
+        //원소들을 담을 스텍
+        Stack<String> stack=new Stack<>();
 
-        if(arrayList.size()==str.length()){
-            for (int i=0;i<arrayList.size();i++){
+        //1. 중복 제거
+        String NotDupleStr=duplicated(str);
 
-            }
-        }
+        //2. 원소를 고르는 과정
+        arrayList=pickOrNot(stack,0,NotDupleStr,arrayList);
 
+
+        //결과 값을 위해 오름차순으로
+        Collections.sort(arrayList);
 
 
         return arrayList;
     }
+
+    public static String duplicated(String str){
+        String result="";
+        //중복 판별
+        for(int i=0;i<str.length();i++){
+            if(str.indexOf(str.charAt(i))==i){
+                result+=str.charAt(i);
+            }
+        }
+
+        String[] sorted=result.split("");
+        Arrays.sort(sorted);
+
+        result=String.join(",",sorted).replaceAll(",","");
+        return result;
+    }
+
+    public static ArrayList<String> pickOrNot(Stack<String> stack, int idx,String subset,ArrayList<String> result){
+
+        //재귀함수 탈출 조건
+        //idx
+        if(idx>=subset.length()){
+            result.add(stack.toString()
+                            .replaceAll("\\[","")
+                            .replaceAll(",","")
+                            .replaceAll("]","")
+                            .replaceAll(" ",""));
+            return result;
+        } else {
+            stack.push(Character.toString(subset.charAt(idx)));
+            pickOrNot(stack,idx+1,subset,result);
+
+            //idx가 부분집합에 포함되지 않은 경우
+            stack.pop();
+            pickOrNot(stack,idx+1,subset,result);
+
+        }
+        return result;
+    }
+
 
 }
